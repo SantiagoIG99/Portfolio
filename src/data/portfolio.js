13b5f9d64f6fbca219829bae5e59a7724a1c8179
Santiago <san99ig@gmail.com>
2023-09-13 19:50:30 -0300

    first commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,16 @@
+export default[
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: '',
+    },
+
+    {
+    title: 'Bank Project',
+    imgUrl: '/assets/Bank.jpg',
+    stack: ['React', 'Tailwind', 'Vite'],
+    link: 'https://banco-proyecto-final.vercel.app/',
+    },
+
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year:'2023',
+        title:'Freelancer',
+        duration:'1 Year',
+        details:"Continued with the learning process of Web Development, with the implementation of Back-End. Not a master on those fields, but gaining knowledge in languages such as Sql, MySql, Typescript, and MongoDB. Overall improving my skills in the creation of Websites",
+    },
+    {
+        year:'2022',
+        title:'Data Entry/Website Builder',
+        duration:'6 Months',
+        details:"I worked as a Data Entry/Website Builder in Luxury Presence helping with the creation of websites for Real Estate clients. We used a CMS similar to Wordpress/Wix, implementing HTML and CSS, whit the occasional use of JavaScript. I also modified images to implement it to the website using tools such as Photoshop or online sites such as Befunky. I worked with defined SOP's but sometimes had to implement changes from my own personal taste.",
+    },
+    {
+        year:'2021',
+        title:'Freelancer',
+        duration:'1 Year',
+        details:'First lessons of coding and learning basic Front-End development with online courses and small projects/practices.'
+    }
+]
